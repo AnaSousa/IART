d399dd4cb94e6ee7502f806dca164b7896f3e801
AnaSousa <ei11068@fe.up.pt>
2014-04-12 21:16:23 +0100

    added addVertex and addEdge to graph class

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -11,6 +11,28 @@ public class Graph extends DirectedWeightedPseudograph<String, Edge>{
 		super((Class<? extends Edge>) edgeClass);
 	}
 
+	public boolean addVertex() {
+		Node n=new Node();
+		return addVertex(n.getStringId());
+	}
+	
+	public boolean addVertex(int type) {
+		Node n=new Node(type);
+		return addVertex(n.getStringId());
+	}
+	
+	public boolean addVertex(Node node) {
+		return addVertex(node.getStringId());
+	}
+
+	public boolean addEdge(String from, String to, int weight) {
+		Edge e=new Edge(from, to, weight);
+		return addEdge(e.getSource(), e.getTarget(), e);
+	}
+	
+	public boolean addEdge(Edge edge) {
+		return addEdge(edge.getSource(), edge.getTarget(), edge);
+	}
 	
 	public void calculateDistances() {
 		//calcultateDistanceDump();
