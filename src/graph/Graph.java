a9d13b8eb7e7f93325dab6071f9fa3c6a7071c2d
AnaSousa <ei11068@fe.up.pt>
2014-04-14 11:38:26 +0100

    added position to node, improved graph class

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,12 +1,12 @@
 package graph;
 
+import java.util.Set;
+
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DirectedWeightedPseudograph;
 
 @SuppressWarnings("serial")
 public class Graph extends DirectedWeightedPseudograph<String, Edge>{
-
-	//TODO: criar lista para guardar vertices?
 	
 	@SuppressWarnings("unchecked")
 	public Graph(Class<? extends DefaultEdge> edgeClass) {
@@ -26,14 +26,28 @@ public class Graph extends DirectedWeightedPseudograph<String, Edge>{
 	public boolean addVertex(Node node) {
 		return addVertex(node.getStringId());
 	}
-
+	
 	public boolean addEdge(String from, String to, int weight) {
 		Edge e=new Edge(from, to, weight);
 		return addEdge(e.getSource(), e.getTarget(), e);
 	}
 	
+	public boolean addEdge(Node n1, Node n2) {
+		Edge e=new Edge(n1.getStringId(), n2.getStringId());
+		return addEdge(e.getSource(), e.getTarget(), e);
+	}
+	
+	public boolean addEdge(Node n1, Node n2, int weight) {
+		Edge e=new Edge(n1.getStringId(), n2.getStringId(), weight);
+		return addEdge(e.getSource(), e.getTarget(), e);
+	}
+	
 	public boolean addEdge(Edge edge) {
 		return addEdge(edge.getSource(), edge.getTarget(), edge);
+	}
+	
+	public Set<Edge> getEdges() {
+		return edgeSet();
 	}
 	
 	public void calculateDistances() {
--- src/graph/Node.java
+++ src/graph/Node.java
@@ -14,15 +14,31 @@ public class Node {
 	private int distanceToDump=Integer.MAX_VALUE;
 	private int distanceToPetrolStation=Integer.MAX_VALUE;
 	
+	private int posX;
+	private int posY;
+	
 	public Node() {
 		id=currentId;
 		currentId++;
+	}
+	
+	public Node(int x, int y) {
+		id=currentId;
+		currentId++;
+		setPosition(x, y);
 	}
 	
 	public Node(int type) {
 		id=currentId;
 		currentId++;
 		this.type=type;
+	}
+	
+	public Node(int type, int x, int y) {
+		id=currentId;
+		currentId++;
+		this.type=type;
+		setPosition(x, y);
 	}
 	
 	public int getId() {
@@ -37,6 +53,19 @@ public class Node {
 		return type;
 	}
 	
+	public int getX() {
+		return posX;
+	}
+	
+	public int getY() {
+		return posY;
+	}
+	
+	public void setPosition(int x, int y) {
+		posX=x;
+		posY=y;
+	}
+
 	public int getDistanceToDump() {
 		return distanceToDump;
 	}
