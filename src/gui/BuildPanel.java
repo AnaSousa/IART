93cf4eb86055329f142ece84a90ecc940de84646
Tiago Fernandes <ei11054@fe.up.pt>
2014-04-19 20:56:03 +0100

    can add objects to the map

--- src/gui/BuildPanel.java
+++ src/gui/BuildPanel.java
@@ -1,51 +1,212 @@
 package gui;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
+@SuppressWarnings("serial")
 public class BuildPanel extends JPanel {
-	
+	public static final  int EMPTY = 0;
+	public static final  int STREET = 1;
+	public static final  int GARBAGE_BIN = 2;
+	public static final  int GAS_STATION = 3;
+	public static final  int GARBAGE_DEPOSIT = 4;
+
 	//images needed
 	private BufferedImage cell;
-	
+	private BufferedImage currentImg;
+
+	private BufferedImage roadV;
+	private BufferedImage roadH;
+	private BufferedImage roadL;
+	private BufferedImage roadR;
+	private BufferedImage roadT;
+	private BufferedImage roadB;
+	private BufferedImage roadX;
+	private BufferedImage bin;
+	private BufferedImage gas;
+	private BufferedImage dump;
+
+	private BufferedImage[] icons;
+
 	//other fields
 	private int mapSizeH;
 	private int mapSizeV;
-	
+
 	//constants
-	private final int X = 40;
-	private final int Y = 75;
-	private final int L = 50;
-	
+	private final int X = 20;
+	private final int Y = 20;
+	private int width = 50;
+
+	private int[][] board = null;
+	private int current;
+
+
+	public void mouseHandler(int x, int y) {
+		System.out.printf("(%d,%d)\n", x, y);
+
+		int x1 = X;
+		int x2 = X + width * mapSizeH;
+		int y1 = Y;
+		int y2 = Y + width * mapSizeV;
+
+
+		if( x1 <= x && x <= x2 && y1 <= y && y <= y2) {
+			int i = ((x - x1)/ width ) % mapSizeH;
+			int j = ((y - y1 )/ width ) % mapSizeV;
+
+
+			board[i][j] = current;
+
+
+		}
+		repaint();
+	}
+
 	/**
 	 * Create the panel.
 	 */
 	public BuildPanel() {
-		
+
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg) {
+				if (arg.getButton() == MouseEvent.BUTTON1) {
+					mouseHandler(arg.getX(), arg.getY());
+				}
+			}
+
+
+		});
+
+
 		mapSizeH = 10;
-		mapSizeV = 5;
+		mapSizeV = 10;
+
+		initBoard();
+		readImages();
+
+		current = STREET;
+		currentImg = icons[STREET];
+	}
+
+	private void initBoard() {
+
+		int copy[][] = board != null ? board.clone() : null;
+
+
+		board = new int[mapSizeH][mapSizeV];
+
+		for(int i = 0; i < board.length; i++)
+			for(int j = 0; j < board[i].length; j++)
+				board[i][j] = EMPTY;
+
+		if(copy != null) {
+			for(int i = 0; i < board.length; i++)
+				for(int j = 0; j < board[i].length; j++) {
+					try {
+						board[i][j] = copy[i][j];
+					}
+					catch(ArrayIndexOutOfBoundsException x) {
+						continue;
+					}
+				}
+		}
+
+	}
+
+	private void readImages() {
 		try {
 			cell = ImageIO.read(new File("resources/cell.png"));
+			roadV = ImageIO.read(new File("resources/roadV.png"));
+			roadH = ImageIO.read(new File("resources/roadH.png"));
+			roadR = ImageIO.read(new File("resources/roadR.png"));
+			roadL = ImageIO.read(new File("resources/roadL.png"));
+			roadT = ImageIO.read(new File("resources/roadT.png"));
+			roadB = ImageIO.read(new File("resources/roadB.png"));
+
+			icons = new BufferedImage[5];
+			icons[STREET] = ImageIO.read(new File("resources/roadX.png"));
+			icons[GARBAGE_BIN] = ImageIO.read(new File("resources/bin.png"));
+			icons[GARBAGE_DEPOSIT] = ImageIO.read(new File("resources/dump.png"));
+			icons[GAS_STATION] = ImageIO.read(new File("resources/gas.png"));
+			icons[EMPTY] = cell;
+
+
+
+			roadX = icons[STREET];
+			bin = icons[GARBAGE_BIN];
+			gas = icons[GAS_STATION];
+			dump = icons[GARBAGE_DEPOSIT];
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
+		/*for(int i = 0; i < mapSizeH; i++) {
+			for(int j = 0; j < mapSizeV; j++)
+				g.drawImage(cell, X + i * width, Y + j * width, width, width, null);
+		}*/
+
 		for(int i = 0; i < mapSizeH; i++) {
 			for(int j = 0; j < mapSizeV; j++)
-				g.drawImage(cell, X + i * L, Y + j * L, L, L, null);
+				g.drawImage(icons[board[i][j]], X + i * width, Y + j * width, width, width, null);
 		}
-		
-	//	g.drawImage(cell, 80, 5,30,30,null);
+
+
+	}
+
+	public void setMapSize(int h, int v) {
+		mapSizeH = h;
+		mapSizeV = v;
+
+
+		//size of the screen
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		//height of the task bar
+		Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration());
+		int taskBarSize = scnMax.bottom;
+
+		//available size of the screen 
+		//setLocation(screenSize.width - getWidth(), screenSize.height - taskBarSize - getHeight());
+
+		int maxY = screenSize.height - taskBarSize*2 /*- getHeight()*/ - Y;
+		int maxX = screenSize.width /*- getWidth()*/ - X - 160;
+		System.out.printf("screenSize.height: %d   taskBarSize: %d   getHeight(): %d\n", screenSize.height, taskBarSize,getHeight());
+		System.out.printf("MaxX: %d   MaxY: %d\n", maxX, maxY);
+
+		System.out.println(maxX / mapSizeH);
+
+		//if(max / mapSizeH < L)
+
+		int mH = (int) Math.floor(maxX / mapSizeH) < 50 ? (int) Math.floor(maxX / mapSizeH) : 50;
+		int mV = (int) Math.floor(maxY / mapSizeV) < 50 ? (int) Math.floor(maxY / mapSizeV) : 50;
+
+		width = mH < mV ? mH : mV;
+
+		initBoard();
+		repaint();
+	}
+
+	public void setIcon(int modo) {
+		current = modo;
+		currentImg = icons[modo];
 	}
 
 }
--- src/gui/BuildWindow.java
+++ src/gui/BuildWindow.java
@@ -1,17 +1,43 @@
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+@SuppressWarnings("serial")
 public class BuildWindow extends JFrame {
 
 	private JPanel contentPane;
 	private BuildPanel panel;
+	private JLabel lblVertical;
+	private JLabel lblHorizontal;
+	private JSlider sliderV;
+	private JSlider sliderH;
+	private JRadioButton rdbtnPetrolStation;
+	private JRadioButton rdbtnGarbageDeposit;
+	private JRadioButton rdbtnGarbageBin;
+	private JRadioButton rdbtnStreet;
 
 	/**
 	 * Launch the application.
@@ -33,22 +59,135 @@ public class BuildWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public BuildWindow() {
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent arg0) {
+				Component c = (Component) arg0.getSource();
+
+				panel.setSize(c.getSize().width - 111, c.getSize().height);
+			}
+		});
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 610);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.WEST);
-		
-		JButton btnCenas = new JButton("Cenas");
-		panel.add(btnCenas);
-		
+		contentPane.setLayout(null);
+
 		panel = new BuildPanel();
-		this.add(panel);
-		
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				panel.mouseHandler(e.getX(), e.getY());
+			}
+		});
+		panel.setBounds(135, 11, 439, 550);
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		getContentPane().add(panel);
+
+		JPanel toolsPanel = new JPanel();
+		toolsPanel.setBounds(10, 11, 122, 550);
+		contentPane.add(toolsPanel);
+		toolsPanel.setLayout(null);
+
+		JLabel lblSize = new JLabel("Size");
+		lblSize.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblSize.setBounds(10, 11, 46, 14);
+		toolsPanel.add(lblSize);
+
+		lblVertical = new JLabel("10");
+		lblVertical.setHorizontalAlignment(SwingConstants.CENTER);
+		lblVertical.setBounds(31, 42, 29, 14);
+		toolsPanel.add(lblVertical);
+
+		lblHorizontal = new JLabel("10");
+		lblHorizontal.setHorizontalAlignment(SwingConstants.CENTER);
+		lblHorizontal.setBounds(72, 79, 28, 14);
+		toolsPanel.add(lblHorizontal);
+
+
+		sliderH = new JSlider();
+		sliderH.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				lblHorizontal.setText(Integer.toString(sliderH.getValue()));
+			}
+		});
+		sliderH.setValue(10);
+		sliderH.setBounds(16, 104, 84, 23);
+		sliderH.setMinimum(1);
+		sliderH.setMaximum(69);
+		toolsPanel.add(sliderH);
+
+
+
+		sliderV = new JSlider();
+		sliderV.setValue(10);
+		sliderV.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				lblVertical.setText(Integer.toString(sliderV.getValue()));
+			}
+		});
+		sliderV.setOrientation(SwingConstants.VERTICAL);
+		sliderV.setBounds(0, 36, 37, 70);
+		sliderV.setMinimum(1);
+		sliderV.setMaximum(40);
+		toolsPanel.add(sliderV);
+
+		Button button = new Button("Update");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				panel.setMapSize(sliderH.getValue(), sliderV.getValue());
+			}
+		});
+		button.setBounds(26, 133, 70, 22);
+		toolsPanel.add(button);
+
+		JLabel lblObjects = new JLabel("Objects");
+		lblObjects.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblObjects.setBounds(10, 184, 70, 14);
+		toolsPanel.add(lblObjects);
+
+		rdbtnStreet = new JRadioButton("Street");
+		rdbtnStreet.setBounds(6, 205, 109, 23);
+		rdbtnStreet.addItemListener(new HandlerIcon(BuildPanel.STREET));
+		toolsPanel.add(rdbtnStreet);
+
+		rdbtnGarbageBin = new JRadioButton("Garbage bin");
+		rdbtnGarbageBin.setBounds(6, 231, 109, 23);
+		rdbtnGarbageBin.addItemListener(new HandlerIcon(BuildPanel.GARBAGE_BIN));
+		toolsPanel.add(rdbtnGarbageBin);
+
+		rdbtnPetrolStation = new JRadioButton("Gas Station");
+		rdbtnPetrolStation.setBounds(6, 257, 109, 23);
+		rdbtnPetrolStation.addItemListener(new HandlerIcon(BuildPanel.GAS_STATION));
+		toolsPanel.add(rdbtnPetrolStation);
+
+		rdbtnGarbageDeposit = new JRadioButton("Garbage deposit");
+		rdbtnGarbageDeposit.setBounds(6, 283, 123, 23);
+		rdbtnGarbageDeposit.addItemListener(new HandlerIcon(BuildPanel.GARBAGE_DEPOSIT));
+		toolsPanel.add(rdbtnGarbageDeposit);
+
+		ButtonGroup options = new ButtonGroup();
+		options.add(rdbtnGarbageBin);
+		options.add(rdbtnStreet);
+		options.add(rdbtnGarbageDeposit);
+		options.add(rdbtnPetrolStation);
+
+		panel.requestFocus();
+
+	}
+
+	private class HandlerIcon implements ItemListener {
+		private int modo;
+		HandlerIcon(int modo) {
+			this.modo = modo;
+		}
+		public void itemStateChanged(ItemEvent event) {
+			panel.setIcon(modo);
+		}
+
 	}
 
 }
